477676647501f1b2f438eab027c7e3fa2b2cb559
RajatTheDev <RajatTheDev@users.noreply.github.com>
2026-03-13 19:29:13 +0530

    feat: add hamburger menu and make UI responsive

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const toggleBtn = document.getElementById("toggle-theme-toggle")
 const icon = toggleBtn.querySelector("img")
+const hamburger = document.getElementById("hamburger")
+const navLinks = document.querySelector(".nav-links")
 
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme")
@@ -9,4 +11,9 @@ toggleBtn.addEventListener("click", () => {
     } else {
         icon.src = "icons/sun.png"
     }
+})
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navLinks.classList.toggle("active")
 })
